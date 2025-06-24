df6ab4683605d75e22ecd70624e5d7de4b0092e1
Danprog4 <148579547+Danprog4@users.noreply.github.com>
2025-06-24 20:13:28 +0500

    fix: add absolute positioning for decorative line in AfterParty component for improved visual hierarchy

--- src/components/AfterParty.tsx
+++ src/components/AfterParty.tsx
@@ -8,7 +8,8 @@ export const AfterParty = ({ stands }: { stands: Stand[] }) => {
   const queryClient = useQueryClient();
   const isCompleted = getIsZoneCompleted(stands, queryClient);
   return (
-    <div className="rounded-[7px] h-[102px] mt-8 p-4 flex items-center justify-between bg-gradient-to-r from-[#20A261] to-[#1C5C45]">
+    <div className="relative rounded-[7px] h-[102px] mt-8 p-4 flex items-center justify-between bg-gradient-to-r from-[#20A261] to-[#1C5C45]">
+      <div className="absolute -top-4 left-0 right-0 h-1 bg-[#1C5C45] "></div>
       <div className="flex flex-col gap-3">
         <span className="text-white text-2xl font-bold"> After Party</span>
       </div>
