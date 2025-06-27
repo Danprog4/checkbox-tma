c67cb82d1c30a85d36cdb520f07420456f06b540
Danprog4 <148579547+Danprog4@users.noreply.github.com>
2025-06-27 17:25:30 +0500

    ашч

--- src/App.tsx
+++ src/App.tsx
@@ -104,15 +104,15 @@ function App() {
   }
 
   return (
-    <div className="bg-black fixed pt-4 inset-0 px-4 h-full overflow-hidden pb-24">
-      <div className=" relative h-full w-full  rounded-b-none rounded-xl overflow-x-hidden overflow-y-hidden">
+    <div className="bg-black fixed pt-4 inset-0 px-4 overflow-hidden">
+      <div className=" relative h-full w-full max-w-screen rounded-b-none rounded-xl overflow-x-hidden overflow-y-auto">
         {/* <div className="text-center flex justify-center pt-9 pb-7 px-[35px]">
           <div className="text-white text-nowrap text-3xl sm:text-3xl md:text-4xl font-bold uppercase tracking-wide">
             список стендов
           </div>
         </div> */}
 
-        <div className="space-y-10 pb-24">
+        <div className="space-y-10 pb-24 ">
           <div className="space-y-3">
             <div className="space-y-2">
               {(groupedStands.layout ?? []).map(
