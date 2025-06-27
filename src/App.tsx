7c5b03b5c20a5b77c2069bebe78d5949688b40fd
Danprog4 <148579547+Danprog4@users.noreply.github.com>
2025-06-27 19:00:15 +0500

    dd

--- src/App.tsx
+++ src/App.tsx
@@ -108,7 +108,7 @@ function App() {
 
   return (
     <div className="bg-black pt-4 px-4 h-[100vh]">
-      <div className=" h-[20000px]">
+      <div className=" h-[50vh]">
         <div className="space-y-10 pb-24 ">
           <div className="space-y-3">
             <div className="space-y-2">
