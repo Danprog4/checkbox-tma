5e9920829b40f7c1476be72d1d735f474193a16b
Danprog4 <148579547+Danprog4@users.noreply.github.com>
2025-06-27 20:53:20 +0500

    f

--- src/App.tsx
+++ src/App.tsx
@@ -101,7 +101,7 @@ function App() {
   }
 
   return (
-    <div className="flex flex-col h-screen mx-auto items-center bg-black">
+    <div className="flex flex-col h-screen mx-auto items-center justify-center bg-black">
       <div className="space-y-10 pb-24">
         <div className="space-y-3">
           <div className="space-y-2">
